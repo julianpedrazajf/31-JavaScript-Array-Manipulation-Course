f453cfbf7c0889eedba403bb09ed1921a7c58523
julianpedrazajf <julianpedrazajf2@gmail.com>
2024-10-01 10:59:33 +1000

    01Oct24

---
+++ .history/clone course/curso-manipulacion-de-arrays-julian/server/06-reduce-reloaded_20240930093531.js
@@ -0,0 +1,21 @@
+const items = [1, 3, 2, 3];
+
+const rta = items.reduce((obj, item) => {
+    if (!obj[item]){
+        obj[item] = 1;
+    } else {
+        obj[item] = obj[item] + 1;
+    }
+    return obj;
+}, {});
+
+console.log(rta);
+// output
+/* 
+{
+    1: 1,
+    3: 2,
+    2: 1,
+} 
+}, {});
+*/
